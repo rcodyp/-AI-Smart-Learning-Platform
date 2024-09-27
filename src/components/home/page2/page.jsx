e41713a9e7f7c50511a06ac8eeaec870176ce41a
rcodyp <rahul887393@gmail.com>
2024-09-27 11:40:53 +0530

    latest

---
+++ src/components/home/page2/page.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Page2 = () => {
+    return (
+        <div id="page2">
+            <video autoPlay muted loop>
+                <source src="/earth.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            <div id="center">
+                <div>Join Us</div>
+                <div>Revolutionize</div>
+                <div>Education</div>
+                <div>With AI</div>
+            </div>
+         </div>
+    
+
+    
+    );
+};
+
+export default Page2;
